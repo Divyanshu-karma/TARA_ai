f83335f85dfa982a866c4210ea635d0a557515fa
Divyanshu-karma <divyanshuvit1@gmail.com>
2026-03-04 23:32:35 +0530

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WEAVIATE_URL = os.getenv("WEAVIATE_URL")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+
+EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5"
